dcf40b420b98f8ae78f3252814c1b8e3b538cbdc
Anwesh43 <anweshthecool0@gmail.com>
2018-03-09 11:32:03 +0530

    adding view in activity

--- app/src/main/java/ui/anwesome/com/kotlincircleedgeview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincircleedgeview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincircleedgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circleedgeview.CircleEdgeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleEdgeView.create(this, 500, 500)
     }
 }
